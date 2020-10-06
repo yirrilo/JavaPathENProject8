2de2c63e3449e9718ce230443a9a4ab53c1613fe
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-10-06 23:47:45 +0200

    doc: add Javadoc to InternalTestHelper class

--- TourGuide/src/main/java/tourGuide/helper/InternalTestHelper.java
+++ TourGuide/src/main/java/tourGuide/helper/InternalTestHelper.java
@@ -1,15 +1,35 @@
 package tourGuide.helper;
 
+/**
+ * This class only contains one private static attribute, the number of internal
+ * users to create for tests (default value = 100), and its getter & setter.
+ *
+ * @author TripMaster
+ * @author Thierry Schreiner
+ */
 public class InternalTestHelper {
 
-	// Set this default up to 100,000 for testing
-	private static int internalUserNumber = 100;
-	
-	public static void setInternalUserNumber(int internalUserNumber) {
-		InternalTestHelper.internalUserNumber = internalUserNumber;
-	}
-	
-	public static int getInternalUserNumber() {
-		return internalUserNumber;
-	}
+    /**
+     * The number of internal users to create for tests.
+     * (Set this default up to 100,000 for testing)
+     */
+    private static int internalUserNumber = 100;
+
+    /**
+     * Setter of internalUserNumber.
+     *
+     * @param internalUserNumber
+     */
+    public static void setInternalUserNumber(int internalUserNumber) {
+        InternalTestHelper.internalUserNumber = internalUserNumber;
+    }
+
+    /**
+     * Getter of internalUserNumber.
+     *
+     * @return an int
+     */
+    public static int getInternalUserNumber() {
+        return internalUserNumber;
+    }
 }
