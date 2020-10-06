9aa1eb97da94beba8b82b4f28422feb434ccd96a
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-10-06 11:02:39 +0200

    fix: change Locale to Locale.US

--- TourGuide/src/main/java/tourGuide/Application.java
+++ TourGuide/src/main/java/tourGuide/Application.java
@@ -2,12 +2,13 @@ package tourGuide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Locale;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        Locale.setDefault(Locale.US);
     }
 
 }
