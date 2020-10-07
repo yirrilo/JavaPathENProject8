a884c10e0f21d0b291c261e1f9ed7c245e360528
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-10-07 14:56:30 +0200

    doc: add JavaDoc to UserReward

--- TourGuide/src/main/java/tourGuide/domain/UserReward.java
+++ TourGuide/src/main/java/tourGuide/domain/UserReward.java
@@ -3,28 +3,81 @@ package tourGuide.domain;
 import gpsUtil.location.Attraction;
 import gpsUtil.location.VisitedLocation;
 
+/**
+ * This class is used to store the details of a reward.
+ *
+ * @author TripMaster
+ * @author Thierry Schreiner
+ */
 public class UserReward {
 
-	public final VisitedLocation visitedLocation;
-	public final Attraction attraction;
-	private int rewardPoints;
-	public UserReward(VisitedLocation visitedLocation, Attraction attraction, int rewardPoints) {
-		this.visitedLocation = visitedLocation;
-		this.attraction = attraction;
-		this.rewardPoints = rewardPoints;
-	}
-	
-	public UserReward(VisitedLocation visitedLocation, Attraction attraction) {
-		this.visitedLocation = visitedLocation;
-		this.attraction = attraction;
-	}
+    /**
+     * The visitedLocation that gives this reward.
+     */
+    public final VisitedLocation visitedLocation;
 
-	public void setRewardPoints(int rewardPoints) {
-		this.rewardPoints = rewardPoints;
-	}
-	
-	public int getRewardPoints() {
-		return rewardPoints;
-	}
-	
+    /**
+     * The attraction concerned by this reward.
+     */
+    public final Attraction attraction;
+
+    /**
+     * The number of points of this reward.
+     */
+    private int rewardPoints;
+
+    /**
+     * All parameters class constructor.
+     *
+     * @param visitedLocation
+     * @param attraction
+     * @param rewardPoints
+     */
+    public UserReward(VisitedLocation visitedLocation, Attraction attraction,
+            int rewardPoints)
+    {
+        this.visitedLocation = visitedLocation;
+        this.attraction = attraction;
+        this.rewardPoints = rewardPoints;
+    }
+
+    /**
+     * Class constructor without rewardPoints parameter.
+     *
+     * @param visitedLocation
+     * @param attraction
+     */
+    public UserReward(VisitedLocation visitedLocation, Attraction attraction) {
+        this.visitedLocation = visitedLocation;
+        this.attraction = attraction;
+    }
+
+    /**
+     * Setter of rewardPoints.
+     *
+     * @param rewardPoints
+     */
+    public void setRewardPoints(int rewardPoints) {
+        this.rewardPoints = rewardPoints;
+    }
+
+    /**
+     * Getter of rewardPoints.
+     *
+     * @return a int, the number of reward points.
+     */
+    public int getRewardPoints() {
+        return rewardPoints;
+    }
+    
+    /**
+     * Serialization method.
+     */
+    @Override
+    public String toString() {
+        return "UserReward [visitedLocation=" + visitedLocation
+                + ", attraction=" + attraction + ", rewardPoints="
+                + rewardPoints + "]";
+    }
+
 }
